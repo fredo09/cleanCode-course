0a828f63b22b828b37aa471cc392929b5a62a886
fredo09 <fred_15@outlook.com>
2025-05-02 00:43:04 -0600

    agregando patron adaptador para axios "principio Abierto/Cerrado" Solid

--- src/Clean-code/solid/02-open-close-c.ts
+++ src/Clean-code/solid/02-open-close-c.ts
@@ -1,16 +1,27 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 /**
  * The Open/Closed Principle (OCP) states that software entities (classes, modules, functions, etc.) should be open for extension but closed for modification.
  */
 
+/**
+ * ! usar siempre un adaptador para uso de librerias de terceros "Patron adapter"
+ * ! Esto implica solo modificar una parte del codigo existente del software y no al resto
+ */
+
 export class HttpClient {
+	// async get(url: string) {
+	// 	const { data, status } = await axios.get(url);
+	// 	console.log("🚀 ~ HttpClient ~ get ~ {data, status}:", {
+	// 		data,
+	// 		status
+	// 	});
+	// 	return {data, status}
+	// }
+
 	async get(url: string) {
-		const { data, status } = await axios.get(url);
-		console.log("🚀 ~ HttpClient ~ get ~ {data, status}:", {
-			data,
-			status
-		});
-		return {data, status}
+		const resp = await fetch(url);
+		const data = await resp.json()
+		return {data, status: resp.status};
 	}
 }
